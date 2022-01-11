d171dca64600776e619376b69ee3a5b9d639733c
briamorr <78086763+briamorr@users.noreply.github.com>
2022-01-11 13:49:46 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,3 +3,16 @@ output "file" {
   description = "Read file"
   value       = file("/etc/os-release")
 }
+
+
+output "log" {
+  description = "Read file"
+  value       = file("/var/log/messages")
+}
+
+
+output "hostname" {
+  description = "Read file"
+  value       = file("/etc/hostname")
+}
+

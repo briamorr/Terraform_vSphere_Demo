8777e9fbfe41f0397cabe14b904ff01274d9d9c7
briamorr <78086763+briamorr@users.noreply.github.com>
2022-02-23 14:39:56 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,55 @@
 
+variable "vsphere_user" {
+  type = string
+  description = "the username for vsphere"
+  
+}
+variable "vsphere_password" {
+  type = string
+  description = "The password for vsphere"
+}
+variable "vsphere_server" {
+  type = string
+  description = "the hostname or ip address of your vcenter server"
+  
+}
+
+variable "vsphere_datacenter" {
+  type = string
+  description = "the name of the datacenter"
+  
+}
+
+variable "vsphere_datastore" {
+  type = string
+  description = "the name of the datastore"
+  
+}
+
+variable "vsphere_vm_name" {
+  type = string
+  description = "the name of the vm"
+}
+
+variable "vsphere_vm_portgroup" {
+  type = string
+  description = "the name of the portgroup"
+  
+}
+
+variable "vsphere_vm_cpu" {
+  type = number
+  description = "the number of vCpus"
+  default = 1
+}
+
+variable "vsphere_vm_memory" {
+  type = number
+  description = "the amount of memory in MB"
+  default = 2048
+}
+
+variable "vsphere_cluster" {
+  type = string
+  description = "the name of the cluster" 
+}

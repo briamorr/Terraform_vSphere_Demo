6a98809fe0a82ff12391eb68facabf8a96e9b6b8
briamorr <78086763+briamorr@users.noreply.github.com>
2022-03-07 09:25:29 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,14 +13,17 @@ resource "local_file" "foo" {
 
 resource "null_resource" "example2" {
   provisioner "local-exec" {
-    command = "wget elasticsearch.default.svc.cluster.local:9200/_cat/health?format=json -O health.json"
+    command = "wget elasticsearch.default.svc.cluster.local:9200/_cat/health?format=json -O ${path.module}/health.json"
   }
+  depends_on = [
+    local_file.foo
+  ]
 }
 
-data "local_file" "foo" {
+data "local_file" "foo2" {
     filename = "${path.module}/health.json"
 }
 
 output "instance_ip_addr" {
-  value = data.local_file.foo
+  value = data.local_file.foo2
 }

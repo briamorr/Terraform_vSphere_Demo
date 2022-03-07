c3dd8aba4b24c41d4407a5b04444acbf8a3fbed5
briamorr <78086763+briamorr@users.noreply.github.com>
2022-03-07 09:18:44 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,11 @@ terraform {
   }
 }
 
+resource "local_file" "foo" {
+    content     = "foo!"
+    filename = "${path.module}/health.json"
+}
+
 resource "null_resource" "example2" {
   provisioner "local-exec" {
     command = "wget elasticsearch.default.svc.cluster.local:9200/_cat/health?format=json -O health.json"

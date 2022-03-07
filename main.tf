f4265742f6b7103e81f48b4405f2f4c9e4cd02f9
briamorr <78086763+briamorr@users.noreply.github.com>
2022-03-07 09:44:16 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,21 +6,12 @@ terraform {
   }
 }
 
-resource "local_file" "foo" {
-    filename = "/tmp/health.json"
-}
-
 resource "null_resource" "example2" {
   provisioner "local-exec" {
     command = "wget elasticsearch.default.svc.cluster.local:9200/_cat/health?format=json -O /tmp/health.json"
   }
 
 }
-
-output "instance_ip_addr" {
-  value = resource.local_file.foo.content
-}
-
 
 output "instance_ip_addr" {
   value = file("/tmp/health.json")
